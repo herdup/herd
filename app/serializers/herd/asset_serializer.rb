16f36b72d45ecbf5ac4bc00851f549c245ecf77a
Sebastian Bean <sebastian@ginlanemedia.com>
2014-07-15 22:42:45 -0400

    still working on the erd

--- app/serializers/herd/asset_serializer.rb
+++ app/serializers/herd/asset_serializer.rb
@@ -1,6 +1,16 @@
 module Herd
   class AssetSerializer < ActiveModel::Serializer
+    embed :ids
     attributes :id
-    attributes :created_at, :file_name
+    attributes :created_at, :file_name, :file_size, :content_type, :type
+    attributes :width, :height
+    attributes :url
+
+    has_many :child_assets
+    has_one :parent_asset
+
+    def url
+      object.file_url
+    end
   end
 end
