a48742b6bc3a69079ef11f739f4f856f59aa96cb
Sebastian Bean <sebastian@ginlanemedia.com>
2014-08-05 15:07:59 -0400

    replace rake with class

---
+++ lib/herd/zip/base.rb
@@ -0,0 +1,12 @@
+module Herd
+  module Zip
+    class Base
+      def class_from_path(path)
+        path.split('/').join('::').constantize
+      end
+      def path_from_class(klass)
+        klass.to_s.split('::').join('/')
+      end
+    end
+  end
+end
