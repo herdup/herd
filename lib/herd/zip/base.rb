c2f457a87c9571ba74ba59b9fc638f30321ba716
Sebastian Bean <sebastian@ginlanemedia.com>
2014-08-05 15:34:34 -0400

    classify is classy

--- lib/herd/zip/base.rb
+++ lib/herd/zip/base.rb
@@ -2,7 +2,7 @@ module Herd
   module Zip
     class Base
       def class_from_path(path)
-        path.split('/').join('::').constantize
+        path.split('/').classify.constantize
       end
       def path_from_class(klass)
         klass.to_s.split('::').join '/'
