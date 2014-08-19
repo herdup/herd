5b72ec863a9a395fc30007251e92114eb218ec7d
Sebastian Bean <sebastian@ginlanemedia.com>
2014-08-19 16:52:38 -0400

    move transforms into they’re nice little home

--- app/models/herd/transform.rb
+++ app/models/herd/transform.rb
@@ -1,14 +1,26 @@
-#require_dependency 'herd/transforms/'
-
 module Herd
   class Transform < ActiveRecord::Base
+
+    attr_accessor :generate_sync
     has_many :assets, dependent: :destroy
 
     serialize :options, HashWithIndifferentAccess
-    validates_uniqueness_of :options, scope: :type
+    validates_uniqueness_of :name, scope: [:type, :assetable_type, :options]
 
     before_validation -> {
-      self.options = YAML::load(options).with_indifferent_access if options.kind_of? String
+      self.options = YAML::load(options) if options.kind_of? String
+      self.options = options.with_indifferent_access unless options.kind_of? HashWithIndifferentAccess
+    }
+
+    after_save -> {
+      assets.map do |a|
+        a.generate_sync = generate_sync
+        a.generate
+      end if options_changed?
+    }
+
+    after_create -> {
+      TransformExportWorker.perform_async
     }
 
     def self.options_from_string(string)
@@ -17,96 +29,26 @@ module Herd
     end
 
     def self.where_t(params)
-      params[:options] = options_from_string(params[:options]).to_yaml
-      params.delete :type if params[:type].nil?
+      params[:options] = options_from_string(params[:options]).to_yaml if params[:options]
+      params.delete_if {|k,v|v.nil?}
       where(params)
     end
 
-    def self.find_or_create_with_options_string(string)
-      params = {options:string}
+    def self.find_or_create_with_options_string(string,name=nil,assetable_type)
+      params = {
+        options:string,
+        name: name,
+        assetable_type: assetable_type
+      }
       where_t(params).first_or_create
     end
-
 
     def perform
       raise 'subclass this'
     end
 
-  end
-
-  class FfmpegTransform < Transform
-    def self.resize_string_from(o_width,o_height,string)
-      t_width, t_height = string.match(/(\d*)x(\d*)/).captures
-      t_width = o_width.to_f * (t_height.to_f/o_height.to_f) unless t_width.present?
-      t_height = o_height.to_f * (t_width.to_f/o_width.to_f) unless t_height.present?
-
-      "#{t_width.to_i}x#{t_height.to_i}"
-    end
-    def perform(asset)
-      options = self.options.symbolize_keys
-      options[:resolution] = self.class.resize_string_from(asset.width,asset.height,options.delete(:resize)) if options[:resize]
-      out = asset.unique_tmppath(options.delete(:format))
-      asset.ffmpeg.transcode(out, options) { |progress| puts progress }
-      out
+    def computed_asset(asset_or_id)
+      asset_or_id.is_a?(Numeric) ? Asset.find(asset_or_id) : asset_or_id
     end
   end
-
-  class SwirlGif < Transform
-    def perform(asset)
-      iterate = self.options.extract!(:step,:start,:end)
-      transforms = (iterate[:start]..iterate[:end]).to_a.map do |ix|
-        "swirl: -#{ix*iterate[:step]}"
-      end
-
-      # dont need to use child assets here necessarily
-      children = transforms.each_with_index.map do |t,ix|
-        asset.t(t, position:ix)
-      end
-      tmp_dir = Dir::Tmpname.tmpdir
-      out_dir = File.join(tmp_dir,asset.file_name_wo_ext)
-      FileUtils.rm_rf(out_dir)
-      FileUtils.mkdir_p(out_dir)
-      children.each_with_index do |a, ix|
-        out_path = File.join(out_dir,"#{a.file_name_wo_ext}_#{ix}.#{a.file_ext}")
-        FileUtils.cp(a.file_path, out_path)
-      end
-      children.reverse.drop(1).each_with_index do |a, ix|
-        out_path = File.join(out_dir,"#{a.file_name_wo_ext}_#{children.count+ix}.#{a.file_ext}")
-        FileUtils.cp(a.file_path, out_path)
-      end
-      gif_path = "#{out_dir}/#{asset.file_name_wo_ext}.gif"
-      `convert -delay 1 #{out_dir}/*.jpg #{gif_path}`
-
-      # FileUtils.rm_rf(out_dir)
-      gif_path
-    end
-  end
-
-  class MiniMagick < Transform
-    def perform(asset)
-      image = asset.mini_magick
-
-      order = %w{resize background gravity extent}
-      opts = options.keys.sort_by{ |el| order.index(el).to_i }.inject({}){|h,k|h[k]=options[k];h}
-
-      image.combine_options do |c|
-        opts.each do |k,v|
-          case k
-          when 'crop'
-            x_regex = /\+(.*?)\+/
-            y_regex = /\+([0-9]*?)$/
-            x_crop_offset = v[x_regex, 1].to_i / 100.0 * asset.width
-            y_crop_offset = v[y_regex, 1].to_i / 100.0 * asset.height
-            image.send k, v.sub(x_regex, "+#{x_crop_offset.to_i}+").sub(y_regex, "+#{y_crop_offset.to_i}")
-          else
-            c.send k,v
-          end
-        end
-      end
-      out = asset.unique_tmppath(options[:format])
-      image.write out
-      out
-    end
-  end
-
 end
---
+++ app/models/herd/transform/ffmpeg.rb
@@ -0,0 +1,22 @@
+module Herd
+  class Transform::Ffmpeg < Transform
+    def self.resize_string_from(o_width,o_height,string)
+      t_width, t_height = string.match(/(\d*)x(\d*)/).captures
+      t_width = o_width.to_f * (t_height.to_f/o_height.to_f) unless t_width.present?
+      t_height = o_height.to_f * (t_width.to_f/o_width.to_f) unless t_height.present?
+
+      t_height += 1 unless t_height.to_i.even?
+      t_width += 1 unless t_width.to_i.even?
+
+      "#{t_width.to_i}x#{t_height.to_i}"
+    end
+    def perform(asset)
+      options = self.options.symbolize_keys
+      options[:resolution] = self.class.resize_string_from(asset.width,asset.height,options.delete(:resize)) if options[:resize]
+      out = asset.unique_tmppath(options.delete(:format))
+      ap options
+      asset.ffmpeg.transcode(out, options) { |progress| yield progress if block_given? }
+      out
+    end
+  end
+end
