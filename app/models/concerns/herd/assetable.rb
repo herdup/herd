7de80769fbf1047ec71e5b69986cab75f92bb80e
Sebastian Bean <sebastian@ginlanemedia.com>
2014-08-19 16:56:16 -0400

    core changes / updates

--- app/models/concerns/herd/assetable.rb
+++ app/models/concerns/herd/assetable.rb
@@ -4,11 +4,50 @@ module Herd
     extend ActiveSupport::Concern
 
     included do
-      has_many :assets, as: :assetable, class_name: 'Herd::Asset'
       ASSETABLE_MODELS.push(self)
+
+      has_many :assets, -> {master.order(:position)}, as: :assetable, class_name: 'Herd::Asset', dependent: :destroy
+
+      assetable_slug
+    end
+
+    def all_assets
+      assets.unscoped
+    end
+
+    def asset
+      assets.master.take || self.class.missing_assets.take || Asset.new
+    end
+
+    def transforms
+      self.class.transforms
     end
 
     module ClassMethods
+      def transforms
+        Transform.where assetable_type: to_s
+      end
+      def missing_assets
+        Asset.where(assetable_type: to_s, assetable_id: 0).master
+      end
+      def missing_asset=(asset)
+        asset.update_column :assetable_type, to_s
+        asset.update_column :assetable_id, 0
+      end
+      def missing
+        missing_assets.take
+      end
+      def find_by_assetable_slug(slug)
+        find_by assetable_slug_column => slug
+      end
+      def assetable_slug(sym=:slug)
+        define_singleton_method :assetable_slug_column do
+          sym
+        end
+        define_method :assetable_slug do
+          send self.class.assetable_slug_column
+        end
+      end
 
     end
   end
