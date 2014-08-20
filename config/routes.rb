291bbe84367e9e2db6c244650f2b2e68a221f6c1
Sebastian Bean <sebastian@ginlanemedia.com>
2014-08-20 01:18:08 -0400

    try ‘gain

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 # require 'sidekiq-status/web'
 
 Herd::Engine.routes.draw do
-  scope :herd do
+  scope :herd, as: :herd do
     # mount Sidekiq::Web => '/sidekiq'
 
     resources :assets do
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Herd::Engine => "/", as: :herd
+  mount Herd::Engine => "/"
 
   resources :posts
   root to: 'posts#index'
