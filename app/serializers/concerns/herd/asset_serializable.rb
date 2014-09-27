96530486a30634a422c86c423ba896a800446e1f
Sebastian Bean <sebastian@ginlanemedia.com>
2014-09-27 19:50:31 -0400

    may be nice feature?

--- app/serializers/concerns/herd/asset_serializable.rb
+++ app/serializers/concerns/herd/asset_serializable.rb
@@ -1,11 +1,15 @@
 module Herd
   module AssetSerializable
     extend ActiveSupport::Concern
+
     included do
-      has_many :assets, serializer: Herd::AssetSerializer
-      def assets
-        object.assets_missing
-      end
+      has_many :assets, serializer: Herd::AssetSerializer, include: true
+    end
+    def assetable_object
+      object
+    end
+    def assets
+      assetable_object.assets_missing
     end
   end
 end
