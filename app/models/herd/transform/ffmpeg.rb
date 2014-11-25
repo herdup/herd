a5beeb5e8177738ebc67dbe75ce8aaf7e0249944
Sebastian Bean <sebastian@ginlanemedia.com>
2014-11-25 17:35:07 -0500

    2 col updates ya dig

--- app/models/herd/transform/ffmpeg.rb
+++ app/models/herd/transform/ffmpeg.rb
@@ -4,8 +4,8 @@ module Herd
     def parse_ffmpeg_options(options)
       if string = options.delete(:resize)
         t_width, t_height = string.match(/(\d*)x(\d*)/).captures
-        t_width = -2 if t_width.empty?
-        t_height = -2 if t_height.empty?
+        t_width = "trunc(ow/a/2)*2" if t_width.empty?
+        t_height = "trunc(ow/a/2)*2" if t_height.empty?
         #TODO: ensure integers, even?
         options[:custom] ||= ''
         options[:custom] += "-vf scale=#{t_width}:#{t_height}"
