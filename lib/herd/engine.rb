a77f3f73dd5ac4a2095489c875abe16238623598
Dmitri Vassilev <dmitrivassilev@gmail.com>
2015-03-02 16:14:36 -0500

    new branch for removal

--- lib/herd/engine.rb
+++ lib/herd/engine.rb
@@ -51,10 +51,12 @@ module Herd
       app.config.autoload_paths << "#{config.root}/lib"
     end
 
-    initializer :append_herd_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
+    if Rails.env.test?
+      initializer :append_herd_migrations do |app|
+        unless app.root.to_s.match root.to_s
+          config.paths["db/migrate"].expanded.each do |expanded_path|
+            app.config.paths["db/migrate"] << expanded_path
+          end
         end
       end
     end
