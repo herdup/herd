95b2600a49e1b2836a9de5768219c5f1743fb85a
Sebastian Bean <sebbean@gmail.com>
2015-06-16 13:34:36 -0700

    public-read acl s3

--- app/models/concerns/herd/s3_fileable.rb
+++ app/models/concerns/herd/s3_fileable.rb
@@ -69,7 +69,7 @@ module Herd
         elsif value.class.in? [File, Tempfile, ActionDispatch::Http::UploadedFile]
           write_url = self[key].url_for(:write, content_type: content_type).to_s
           require 'typhoeus'
-          Typhoeus::Request.new(write_url, method: :put, body: value.read, headers: { 'content-type' => content_type }).run
+          Typhoeus::Request.new(write_url, method: :put, body: value.read, headers: { 'content-type' => content_type, 'x-amz-acl' => 'public-read' }).run
           puts "Uploaded to: #{key} with content type: #{content_type}"
           set_obj_cache key, value
         else
