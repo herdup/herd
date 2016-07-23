f087f9e60e1208a0e342422470e4a00d4d413b62
Sebastian Bean <sebbean@gmail.com>
2016-07-22 17:01:23 -0700

    split up indexes

--- db/migrate/20160722234053_add_missing_indexes.rb
+++ db/migrate/20160722234053_add_missing_indexes.rb
@@ -1,6 +1,10 @@
 class AddMissingIndexes < ActiveRecord::Migration
   def change
-    add_index :herd_assets, [ :parent_asset_id, :assetable_type, :assetable_id, :transform_id ]
-    add_index :herd_transforms, [ :name, :assetable_type ]
+    add_index :herd_assets, :parent_asset_id
+    add_index :herd_assets, :assetable_type
+    add_index :herd_assets, :assetable_id
+    add_index :herd_assets, :transform_id
+    add_index :herd_transforms, :name
+    add_index :herd_transforms, :assetable_type 
   end
 end
