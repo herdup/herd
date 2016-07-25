cb03d308e02531b098081f9171a3cbdcc39d068c
Sebastian Bean <sebbean@gmail.com>
2016-07-25 17:42:17 -0400

    serialize child assets into assets key

--- app/serializers/herd/asset_serializer.rb
+++ app/serializers/herd/asset_serializer.rb
@@ -1,16 +1,20 @@
 module Herd
   class AssetSerializer < ActiveModel::Serializer
-    attributes :id
-    attributes :created_at, :updated_at, :file_name, :file_size, :content_type, :type, :asset_class
-    attributes :width, :height
-    attributes :url
-    attributes :position
-    attributes :metadata
-    attributes :assetable_type, :assetable_id
+    attributes :id,
+     :created_at, :updated_at, 
+     :file_name, 
+     :file_size, 
+     :content_type, 
+     :type, 
+     :asset_class,
+     :width, :height,
+     :url,
+     :position,
+     :metadata,
+     :assetable_type, :assetable_id
 
-    has_many :child_assets,  embed: :ids, include: true, serializer: AssetSerializer
-    has_one :parent_asset,  embed: :ids
-
+    has_many :child_assets, embed: :ids, include: true, serializer: AssetSerializer, root: :assets
+    has_one :parent_asset, embed: :ids, include: false
     has_one :transform,  embed: :ids, include: true, serializer: TransformSerializer
 
     attributes :transform_name
