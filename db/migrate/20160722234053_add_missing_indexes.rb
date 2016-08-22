9fd4b6d2d9f5e16a34d1f8ec07071185feddd259
Sebastian Bean <sebbean@gmail.com>
2016-08-22 15:40:18 -0700

    duplicate index

--- db/migrate/20160722234053_add_missing_indexes.rb
+++ db/migrate/20160722234053_add_missing_indexes.rb
@@ -1,6 +1,5 @@
 class AddMissingIndexes < ActiveRecord::Migration
   def change
-    add_index :herd_assets, :parent_asset_id
     add_index :herd_assets, :assetable_type
     add_index :herd_assets, :assetable_id
     add_index :herd_assets, :transform_id
